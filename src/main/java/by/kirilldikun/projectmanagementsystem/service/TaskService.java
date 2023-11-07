e0b1f416b7df1c97a4686344fd178a5fde2509be
krlld <kirilldikun25@gmail.com>
2023-11-07 20:11:52 +0300

    Add task service

---
+++ src/main/java/by/kirilldikun/projectmanagementsystem/service/TaskService.java
@@ -0,0 +1,85 @@
+package by.kirilldikun.projectmanagementsystem.service;
+
+import by.kirilldikun.projectmanagementsystem.dto.TaskDto;
+import by.kirilldikun.projectmanagementsystem.entity.Task;
+import by.kirilldikun.projectmanagementsystem.exception.TaskAlreadyExistsException;
+import by.kirilldikun.projectmanagementsystem.exception.TaskNotFoundException;
+import by.kirilldikun.projectmanagementsystem.repository.TaskRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    private final ProjectService projectService;
+
+    public Page<TaskDto> findAll(String name, Pageable pageable) {
+        Page<Task> tasks = taskRepository.findAllByNameContainsIgnoreCase(name, pageable);
+        return tasks.map(this::mapToTaskDto);
+    }
+
+    public TaskDto findById(Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        return mapToTaskDto(task);
+    }
+
+    public TaskDto save(@Valid TaskDto taskDto) {
+        if (taskDto.getId() != null && taskRepository.existsById(taskDto.getId())) {
+            throw new TaskAlreadyExistsException();
+        }
+        Task task = mapToTask(taskDto);
+        task = taskRepository.save(task);
+        return mapToTaskDto(task);
+    }
+
+    public TaskDto update(Long id, @Valid TaskDto taskDto) {
+        if (!taskRepository.existsById(id)) {
+            throw new TaskNotFoundException();
+        }
+        Task task = mapToTask(taskDto);
+        task.setId(id);
+        task = taskRepository.save(task);
+        return mapToTaskDto(task);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new TaskNotFoundException();
+        }
+        taskRepository.softDeleteById(id);
+    }
+
+    public Task mapToTask(TaskDto taskDto) {
+        return Task.builder()
+                .id(taskDto.getId())
+                .name(taskDto.getName())
+                .description(taskDto.getDescription())
+                .startAt(taskDto.getStartAt())
+                .endAt(taskDto.getEndAt())
+                .status(taskDto.getStatus())
+                .project(projectService.mapToProject(projectService.findById(taskDto.getProjectId())))
+                .build();
+    }
+
+    public TaskDto mapToTaskDto(Task task) {
+        return TaskDto.builder()
+                .id(task.getId())
+                .name(task.getName())
+                .description(task.getDescription())
+                .startAt(task.getStartAt())
+                .endAt(task.getEndAt())
+                .status(task.getStatus())
+                .projectId(task.getProject().getId())
+                .build();
+    }
+}
