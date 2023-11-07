1224456ae85dd2e091941d1438e92fb99780b160
krlld <kirilldikun25@gmail.com>
2023-11-07 20:01:04 +0300

    Add task repository

---
+++ src/main/java/by/kirilldikun/projectmanagementsystem/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package by.kirilldikun.projectmanagementsystem.repository;
+
+import by.kirilldikun.projectmanagementsystem.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findAllByNameContainsIgnoreCase(String name, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Task t SET t.deleted = true WHERE t.id = :id")
+    void softDeleteById(@Param("id") Long id);
+}
