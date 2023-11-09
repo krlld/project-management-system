560e156f3fdc25a2550d7a1991608f8018c8b138
krlld <kirilldikun25@gmail.com>
2023-11-09 16:57:27 +0300

    Add record classes for authentication request and response

---
+++ src/main/java/by/kirilldikun/projectmanagementsystem/dto/AuthenticationRequest.java
@@ -0,0 +1,4 @@
+package by.kirilldikun.projectmanagementsystem.dto;
+
+public record AuthenticationRequest(String username, String password) {
+}
---
+++ src/main/java/by/kirilldikun/projectmanagementsystem/dto/AuthenticationResponse.java
@@ -0,0 +1,4 @@
+package by.kirilldikun.projectmanagementsystem.dto;
+
+public record AuthenticationResponse(String token) {
+}
