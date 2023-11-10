79947b00cf633761097ada0a357a6e4f7ba586d0
krlld <kirilldikun25@gmail.com>
2023-11-10 19:06:26 +0300

    Add open api documentation for task dto

--- src/main/java/by/kirilldikun/projectmanagementsystem/dto/TaskDto.java
+++ src/main/java/by/kirilldikun/projectmanagementsystem/dto/TaskDto.java
@@ -1,6 +1,7 @@
 package by.kirilldikun.projectmanagementsystem.dto;
 
 import by.kirilldikun.projectmanagementsystem.entity.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,27 +15,36 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Task data")
 public class TaskDto {
 
+    @Schema(description = "Task id", accessMode = Schema.AccessMode.READ_ONLY, example = "1")
     private Long id;
 
     @NotNull
     @NotBlank
+    @Schema(description = "Task name", example = "Design Mockups")
     private String name;
 
     @NotNull
     @NotBlank
+    @Schema(description = "Task description",
+            example = "Create UI mockups based on client requirements")
     private String description;
 
     @NotNull
+    @Schema(description = "Task start date", example = "2023-01-10")
     private LocalDate startAt;
 
     @NotNull
+    @Schema(description = "Task end date", example = "2023-01-20")
     private LocalDate endAt;
 
     @NotNull
+    @Schema(description = "Task status", example = "IN_PROGRESS")
     private Status status;
 
     @NotNull
+    @Schema(description = "Project id", example = "1")
     private Long projectId;
 }
