6aff3dca98e7c18d7125d0bf4de678d352e9cfa9
krlld <kirilldikun25@gmail.com>
2023-11-10 19:13:07 +0300

    Add open api security configuration

--- src/main/java/by/kirilldikun/projectmanagementsystem/configuration/OpenApiConfiguration.java
+++ src/main/java/by/kirilldikun/projectmanagementsystem/configuration/OpenApiConfiguration.java
@@ -1,8 +1,10 @@
 package by.kirilldikun.projectmanagementsystem.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -13,6 +15,12 @@ import io.swagger.v3.oas.annotations.info.Info;
                         email = "kirilldikun25@gmail.com"
                 )
         )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class OpenApiConfiguration {
 }
