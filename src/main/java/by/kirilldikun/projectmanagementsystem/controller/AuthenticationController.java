d68e2037c8689a6629badec672ad9370dcc8b3e4
krlld <kirilldikun25@gmail.com>
2023-11-10 18:07:05 +0300

    Add open api documentation for authentication controller

--- src/main/java/by/kirilldikun/projectmanagementsystem/controller/AuthenticationController.java
+++ src/main/java/by/kirilldikun/projectmanagementsystem/controller/AuthenticationController.java
@@ -3,6 +3,9 @@ package by.kirilldikun.projectmanagementsystem.controller;
 import by.kirilldikun.projectmanagementsystem.dto.AuthenticationRequest;
 import by.kirilldikun.projectmanagementsystem.dto.AuthenticationResponse;
 import by.kirilldikun.projectmanagementsystem.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication controller", description = "Authenticate the user in the system")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
     @GetMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody AuthenticationRequest authenticationRequest) {
+    @Operation(summary = "Register", description = "Allows the user to register in the system")
+    public ResponseEntity<AuthenticationResponse> register(
+            @Valid @RequestBody @Parameter(description = "User data", required = true)
+            AuthenticationRequest authenticationRequest) {
         return ResponseEntity.ok(authenticationService.register(authenticationRequest));
     }
 
     @GetMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest authenticationRequest) {
+    @Operation(summary = "Authenticate", description = "Allows the user to authenticate in the system")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @Valid @RequestBody @Parameter(description = "User data", required = true)
+            AuthenticationRequest authenticationRequest) {
         return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
     }
 }
