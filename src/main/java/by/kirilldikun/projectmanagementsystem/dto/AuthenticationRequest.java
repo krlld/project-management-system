1f51957acd54641026b1d0438b8351a834c042b5
krlld <kirilldikun25@gmail.com>
2023-11-10 18:44:58 +0300

    Add open api documentation for authentication request

--- src/main/java/by/kirilldikun/projectmanagementsystem/dto/AuthenticationRequest.java
+++ src/main/java/by/kirilldikun/projectmanagementsystem/dto/AuthenticationRequest.java
@@ -1,9 +1,11 @@
 package by.kirilldikun.projectmanagementsystem.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "User data for authentication")
 public record AuthenticationRequest(
-        @NotNull @NotBlank String username,
-        @NotNull @NotBlank String password) {
+        @NotNull @NotBlank @Schema(description = "Username") String username,
+        @NotNull @NotBlank @Schema(description = "Password") String password) {
 }
