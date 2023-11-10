a434a3caba16fc753c8cf027c1b58cb03f90a4c6
krlld <kirilldikun25@gmail.com>
2023-11-10 17:50:36 +0300

    Add open api configuration

---
+++ src/main/java/by/kirilldikun/projectmanagementsystem/configuration/OpenApiConfiguration.java
@@ -0,0 +1,18 @@
+package by.kirilldikun.projectmanagementsystem.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Project management system Api",
+                description = "Project management system", version = "1.0.0",
+                contact = @Contact(
+                        name = "Dikun Kirill",
+                        email = "kirilldikun25@gmail.com"
+                )
+        )
+)
+public class OpenApiConfiguration {
+}
