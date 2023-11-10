f35b48b4ad3b0f1459a1bff7d160d94a260bdd7b
krlld <kirilldikun25@gmail.com>
2023-11-10 19:02:00 +0300

    Add open api documentation for project dto

--- src/main/java/by/kirilldikun/projectmanagementsystem/dto/ProjectDto.java
+++ src/main/java/by/kirilldikun/projectmanagementsystem/dto/ProjectDto.java
@@ -1,6 +1,7 @@
 package by.kirilldikun.projectmanagementsystem.dto;
 
 import by.kirilldikun.projectmanagementsystem.entity.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -16,28 +17,37 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Project data")
 public class ProjectDto {
 
+    @Schema(description = "Project id", accessMode = Schema.AccessMode.READ_ONLY, example = "1")
     private Long id;
 
     @NotNull
     @NotBlank
+    @Schema(description = "Project name", example = "Project Alpha")
     private String name;
 
     @NotNull
     @NotBlank
+    @Schema(description = "Project description",
+            example = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
     private String description;
 
     @NotNull
     @Positive
+    @Schema(description = "Project budget", example = "50000.00")
     private BigDecimal budget;
 
     @NotNull
+    @Schema(description = "Project start date", example = "2023-01-01")
     private LocalDate startAt;
 
     @NotNull
+    @Schema(description = "Project end date", example = "2023-03-15")
     private LocalDate endAt;
 
     @NotNull
+    @Schema(description = "Project status", example = "IN_PROGRESS")
     private Status status;
 }
